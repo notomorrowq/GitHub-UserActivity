a47d9365703f55e8a8e8ff2eeade16f91220a496
Oleksandr Novik-Dichko <oleksandrnovikdichko@Oleksandrs-MacBook-Pro.local>
2024-11-02 21:04:50 +0200

    fix duplicate db problem, add some functions

--- db_manger.py
+++ db_manger.py
@@ -3,7 +3,7 @@ import github_api
 
 conn = sq.connect("github_activity.db")
 cur = conn.cursor()
-
+ 
 cur.execute('''CREATE TABLE IF NOT EXISTS events_table
             (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -24,10 +24,16 @@ def save_user_activity(username):
 def save_events(username, events):
     for event in events:
         with conn:
-            cur.execute(''' 
-                INSERT INTO events_table(username, event_type, repo_name, event_date, details)
-                VALUES (?, ?, ?, ?, ?)
+            cur.execute('''
+                SELECT 1 FROM events_table 
+                WHERE username = ? AND event_type = ? AND repo_name = ? AND event_date = ? AND details = ?
             ''', (username, event['event_type'], event['repo_name'], event['event_date'], event['details']))
+           
+            if cur.fetchone() is None:  
+                cur.execute(''' 
+                    INSERT INTO events_table(username, event_type, repo_name, event_date, details)
+                    VALUES (?, ?, ?, ?, ?)
+                ''', (username, event['event_type'], event['repo_name'], event['event_date'], event['details']))
 
 def get_last_user_events(username, limit = 10):
     with conn:
@@ -55,3 +61,8 @@ def get_all_user_events(username):
         all_user_events = cur.fetchall()
 
     return all_user_events
+
+
+def drop_db(table_name):
+    with conn: 
+        cur.execute(f'''DROP TABLE {table_name}''')
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import db_manger
 import activity_display
 
+
 def main():
     username = input("Enter username: ")
     db_manger.save_user_activity(username)
     activity_display.display_activity(db_manger.get_all_user_events(username))
+    #db_manger.drop_db("events_table") #drop db
+
+def all_info_by_username(username):
+    all_user_events = db_manger.get_all_user_events(username)
+    for event in all_user_events:
+        print(event)
 
 
 if __name__ == "__main__":
     main()
+    #username = input("Search by username: ")
+    #all_info_by_username(username)
